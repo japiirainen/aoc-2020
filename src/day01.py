90c6be0df9912b5564c550518e375c6174186357
Joona Piirainen <joona.piirainen@gmail.com>
2023-02-28 20:06:03 +0200

    feat: day 01 in python

---
+++ src/day01.py
@@ -0,0 +1,7 @@
+I = set([int(x) for x in open(0).read().splitlines()])
+
+# part 1
+print(next(a * b for a in I for b in I & {2020 - a}))
+
+# part 2
+(print(next(a * b * c for a in I for b in I for c in I & {2020 - a - b})))
