e6a33d86402775f8f1abbcc94cc2df4ab533d23d
Joona Piirainen <joona.piirainen@gmail.com>
2023-03-07 18:24:14 +0200

    feat: day 09 part 1 in python

---
+++ src/day09.py
@@ -0,0 +1,15 @@
+from itertools import combinations
+
+I = [int(x) for x in open(0).read().splitlines()]
+
+preamble_size = 25
+preamble, rest = I[:preamble_size], I[preamble_size:]
+
+def part1():
+    for i, x in enumerate(rest):
+        y = I[:preamble_size + i][-preamble_size:]
+        cs = map(lambda x: x[0] + x[1], combinations(y, 2))
+        if len([() for c in cs if c == x]) == 0:
+            return x
+            
+print(part1())
