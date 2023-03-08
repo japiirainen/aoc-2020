9db2ab6fa0dbe19633bc2573dfdfb73ac4d47043
Joona Piirainen <joona.piirainen@gmail.com>
2023-03-08 20:19:57 +0200

    chore: improve day 9 part 1

--- src/day09.py
+++ src/day09.py
@@ -5,11 +5,16 @@ I = [int(x) for x in open(0).read().splitlines()]
 preamble_size = 25
 preamble, rest = I[:preamble_size], I[preamble_size:]
 
-def part1():
+
+def invalids_():
     for i, x in enumerate(rest):
-        y = I[:preamble_size + i][-preamble_size:]
+        y = I[: preamble_size + i][-preamble_size:]
         cs = map(lambda x: x[0] + x[1], combinations(y, 2))
         if len([() for c in cs if c == x]) == 0:
-            return x
+            yield x
 
-print(part1())
+
+invalids = invalids_()
+
+# part 1
+print(next(invalids))
