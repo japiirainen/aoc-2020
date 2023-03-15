c77d3680959f6488481c73839ec2e937f44675c3
Joona Piirainen <joona.piirainen@gmail.com>
2023-03-15 19:05:51 +0100

    feat: solve day 11 part 2 in python

--- src/day11.py
+++ src/day11.py
@@ -1,31 +1,29 @@
-I = open(0).read().splitlines()
+import sys
 
-I = """
-L.LL.LL.LL
-LLLLLLL.LL
-L.L.L..L..
-LLLL.LL.LL
-L.LL.LL.LL
-L.LLLLL.LL
-..L.L.....
-LLLLLLLLLL
-L.LLLLLL.L
-L.LLLLL.LL
-""".strip().splitlines()
+I = [list(s) for s in open(0).read().splitlines()]
 
-
-I = [list(s) for s in I]
-
-occupied = "#"
-empty = "L"
+occupied, empty, floor, bad = "#L.?"
+deltas = ((-1, -1), (-1, 0), (1, -1), (0, -1), (1, 0), (-1, 1), (0, 1), (1, 1))
 
 
 def neighbors(l, r, c):
     return [
         l[r + d[0]][c + d[1]]
-        for d in ((-1, -1), (-1, 0), (1, -1), (0, -1), (1, 0), (-1, 1), (0, 1), (1, 1))
+        for d in deltas
         if 0 <= r + d[0] < len(l) and 0 <= c + d[1] < len(l[0])
     ]
+
+
+def see(l, r, c):
+    def visible(r, c, dr, dc):
+        for _ in range(1, sys.maxsize):
+            r, c = r + dr, c + dc
+            if not (0 <= r < len(l) and 0 <= c < len(l[0])):
+                return bad
+            if l[r][c] != floor:
+                return l[r][c]
+
+    return [visible(r, c, d[0], d[1]) for d in deltas]
 
 
 def step_1(prev_gen):
@@ -37,6 +35,19 @@ def step_1(prev_gen):
             if cur == empty and n_occupied == 0:
                 next_gen[i][j] = occupied
             elif cur == occupied and n_occupied >= 4:
+                next_gen[i][j] = empty
+    return next_gen
+
+
+def step_2(prev_gen):
+    next_gen = [s[:] for s in prev_gen[:]]
+    for i, row in enumerate(prev_gen):
+        for j, cur in enumerate(row):
+            ss = see(prev_gen, i, j)
+            n_occupied = len([x for x in ss if x == occupied])
+            if cur == empty and n_occupied == 0:
+                next_gen[i][j] = occupied
+            elif cur == occupied and n_occupied >= 5:
                 next_gen[i][j] = empty
     return next_gen
 
@@ -64,3 +75,6 @@ def solve(l, step_f):
 
 # part 1
 print(solve(I, step_1))
+
+# part 2
+print(solve(I, step_2))
