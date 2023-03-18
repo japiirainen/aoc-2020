fa72042d5fecb88829a5d7984bf275679bbf9796
Joona Piirainen <joona.piirainen@gmail.com>
2023-03-18 14:54:54 +0200

    some more bikeshedding on day 13

--- src/day13.py
+++ src/day13.py
@@ -1,13 +1,12 @@
+import math
+from sympy.ntheory.modular import crt
+
 tt, ids = open(0).read().splitlines()
 ids = [x for x in ids.split(",")]
 int_ids = [int(x) for x in ids if x != "x"]
 
-id, mins = min([(x - int(tt) % x, x) for x in int_ids])
-
 # part 1
-print(id * mins)
+print(math.prod(min([(x - int(tt) % x, x) for x in int_ids])))
 
 # part 2
-from sympy.ntheory.modular import crt
-
 print(crt(int_ids, [-i % int(x) for i, x in enumerate(ids) if x != "x"])[0])
