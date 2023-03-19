b47e7a5165bc8529c80151924a5b240eb3f174f2
Joona Piirainen <joona.piirainen@gmail.com>
2023-03-19 12:48:02 +0200

    chore: refactor day 14

--- src/day14.py
+++ src/day14.py
@@ -1,5 +1,5 @@
 import re
-from itertools import product
+import itertools
 
 
 def parseline(line: str):
@@ -15,33 +15,30 @@ def bin36(i: int) -> str:
     return f"{i:036b}"
 
 
-def exec_v1(instrs):
-    mask = bin36(0)
-    mem = {}
-    for op, arg in instrs:
-        if op == "mask":
-            mask = arg
-        else:
-            addr, val = op, bin36(arg)
-            val = "".join([m if m in "10" else v for m, v in zip(mask, val)])
-            mem[addr] = int(val, base=2)
-    return mem
+def exec_v1(instrs, mask=bin36(0), mem={}):
+    if len(instrs) == 0:
+        return mem
+    (addr, val), *instrs = instrs
+    if addr == "mask":
+        return exec_v1(instrs, val, mem)
+    mem[addr] = int(
+        "".join([m if m in "01" else v for m, v in zip(mask, bin36(val))]), base=2
+    )
+    return exec_v1(instrs, mask, mem)
 
 
-def exec_v2(instrs):
-    mask = bin36(0)
-    mem = {}
-    for addr, val in instrs:
-        if addr == "mask":
-            mask = val
-        else:
-            addr = "".join(
-                a if m == "0" else "1" if m == "1" else "{}"
-                for m, a in zip(mask, bin36(addr))
-            )
-            for bits in product("01", repeat=addr.count("{")):
-                mem[int(addr.format(*bits), base=2)] = val
-    return mem
+def exec_v2(instrs, mask=bin36(0), mem={}):
+    if len(instrs) == 0:
+        return mem
+    (addr, val), *instrs = instrs
+    if addr == "mask":
+        return exec_v2(instrs, val, mem)
+    addr = "".join(
+        a if m == "0" else "1" if m == "1" else "{}" for m, a in zip(mask, bin36(addr))
+    )
+    for bits in itertools.product("01", repeat=addr.count("{")):
+        mem[int(addr.format(*bits), base=2)] = val
+    return exec_v2(instrs, mask, mem)
 
 
 def solve(mem):
