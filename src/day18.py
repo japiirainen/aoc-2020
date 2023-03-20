a4d952c80e2040904ef795695004708561da04dc
Joona Piirainen <joona.piirainen@gmail.com>
2023-03-20 14:39:13 +0200

    feat: add day 18 part 2 in python

--- src/day18.py
+++ src/day18.py
@@ -20,4 +20,20 @@ def eval_1(expr):
         return x if not rest else eval_1((x, *rest))
 
 
+# part 1
 print(sum(eval_1(tokens) for tokens in I))
+
+
+def eval_2(expr):
+    if isinstance(expr, int):
+        return expr
+    elif "*" in expr:
+        # force multiplication to be evaluated last
+        i = expr.index("*")
+        return eval_2(expr[:i]) * eval_2(expr[i + 1 :])
+    else:
+        return sum(eval_2(x) for x in expr if x not in ops)
+
+
+# part 2
+print(sum(eval_2(tokens) for tokens in I))
