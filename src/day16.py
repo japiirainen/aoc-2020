b19bc75d8138cf1ad5f48daa4ce5ea03840ad109
Joona Piirainen <joona.piirainen@gmail.com>
2023-03-20 09:29:01 +0200

    feat: solve day 16 part 2 in python

--- src/day16.py
+++ src/day16.py
@@ -1,5 +1,7 @@
 from typing import Tuple, List
 from dataclasses import dataclass
+import numpy as np
+import math
 
 Ticket = List[int]
 Rule = List[Tuple[int, int]]
@@ -20,18 +22,19 @@ def parse_input(I):
     }
     parse_ticket = lambda ts: [int(t) for t in ts.split(",")]
     my = parse_ticket(my.splitlines()[-1])
-    nearby = map(parse_ticket, nearby.splitlines()[1:])
+    nearby = list(map(parse_ticket, nearby.splitlines()[1:]))
     return Env(rules=rules, my=my, nearby=nearby)
 
 
 env = parse_input(open(0).read())
 
 
+is_valid = lambda lo, hi, v: lo <= v <= hi
+
+
 def find_invalid(t: Ticket, rules: List[Rule]):
     for val in t:
-        is_invalid = lambda rs: all(
-            (lambda lo, hi: val < lo or val > hi)(*r) for r in rs
-        )
+        is_invalid = lambda rs: all(not is_valid(*r, val) for r in rs)
         if len([val for rule in rules.values() if is_invalid(rule)]) == len(t):
             return val
 
@@ -46,3 +49,29 @@ def invalid_tickets(env: Env):
 
 # part 1
 print(sum(invalid_tickets(env)))
+
+
+def valid_rules(vs: List[int], rules: List[Rule]):
+    rule_applies = lambda rule, v: any(is_valid(*r, v) for r in rule)
+    return [
+        name for name, rule in rules.items() if all(rule_applies(rule, v) for v in vs)
+    ]
+
+
+def find_valid_fields(env: Env, valids: List[Ticket]):
+    valids = np.array(valids).T.tolist()
+    valids = enumerate([valid_rules(col, env.rules) for col in valids])
+    valids = sorted(valids, key=lambda s: len(s[1]))
+    for i in range(1, len(valids)):
+        item = valids[i - 1][1][0]
+        for j in range(i, len(valids)):
+            valids[j][1].remove(item)
+    ids = [idx for (idx, field) in valids if field[0].startswith("departure")]
+    assert len(ids) == 6, "There must be 6 fields that start with 'departure'"
+    return math.prod(env.my[i] for i in ids)
+
+
+valids = [t for t in env.nearby if find_invalid(t, env.rules) == None]
+
+# part 2
+print(find_valid_fields(env, valids))
